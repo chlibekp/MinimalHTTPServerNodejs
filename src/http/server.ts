f0525132e35be459badf6edaf5db2791f9a6a10a
Hegy <45619085+hegyok@users.noreply.github.com>
2025-11-07 22:44:58 +0100

    basic route handler

--- src/http/server.ts
+++ src/http/server.ts
@@ -9,8 +9,10 @@ import HTTPHeaders from './headers';
 
 class HTTPServer extends (EventEmitter as new () => TypedEmitter<HTTPServerEvents>) {
 	tcpServer: TCPServer;
+	private routes: Map<string, Function>;
 	constructor(port: number) {
 		super();
+		this.routes = new Map<string, Function>();
 		this.tcpServer = new TCPServer(port);
 	}
 
@@ -22,7 +24,11 @@ class HTTPServer extends (EventEmitter as new () => TypedEmitter<HTTPServerEvent
 		});
 
 		// Handle new TCP connections
-		this.tcpServer.on("connection", this.handleConnection)
+		this.tcpServer.on("connection", this.handleConnection.bind(this));
+	}
+
+	route(method: string, path: string, handler: (req: RequestFactory, res: ResponseFactory) => void) {	
+		this.routes.set(`${method}${path}`, handler);
 	}
 
 	handleConnection(socket: net.Socket) {
@@ -30,6 +36,19 @@ class HTTPServer extends (EventEmitter as new () => TypedEmitter<HTTPServerEvent
 			const request = new RequestFactory()
 				.setBuffer(data)
 				.build();
+			const url = new URL(`http://localhost${request.path}`)
+			const callback = this.routes.get(`${request.method}${url.pathname}`)
+			try {
+				callback?.(request, new ResponseFactory(socket))
+			} catch (error) {
+				const response = new ResponseFactory(socket);
+				response.writeHeader(500);
+				const headers = new HTTPHeaders();
+				headers.set("Content-Type", "text/plain");
+				response.sendHeaders(headers)
+				response.write("Internal Server Error");
+				response.end();
+			}
 		});
 	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import HTTPServer from "./http/server.ts";
+import HTTPHeaders from "./http/headers";
+import HTTPServer from "./http/server";
 
 const server = new HTTPServer(3000);
 
@@ -6,4 +7,13 @@ server.listen();
 
 server.on("listening", (port: number) => {
 	console.log(`HTTP Server is listening on port ${port}`);
+})
+
+server.route("GET", "/", (req, res) => {
+	res.writeHeader(200);
+	const headers = new HTTPHeaders();
+	headers.set("Content-Type", "application/json");
+	res.sendHeaders(headers)
+	res.write(JSON.stringify({ message: "Hello, World!" }));
+	res.end();
 })
